c34567f3d30ef4af8c118e817031b0d7b3a92001
agnieszka-pytel <agnieszkapytel25@gmail.com>
2020-03-16 17:04:35 +0100

    Added header menu

--- src/components/layout/Header/HeaderMenu.js
+++ src/components/layout/Header/HeaderMenu.js
@@ -1,7 +1,43 @@
 import React, { Component } from 'react';
+import Link from '@src/components/ui/Link/';
+import Logo from '@src/assets/img/logo.png';
+import SearchIcon from '@src/assets/img/search.png';
+import UserIcon from '@src/assets/img/user.png';
+import CartIcon from '@src/assets/img/cart-ico.png';
+import styles from './HeaderMenu.module';
 
 export default class HeaderMenu extends Component {
   render() {
-    return <div></div>;
+    return (
+      <section className={styles.section}>
+        <figure className={styles.logoWrapper}>
+          <img src={Logo} className={styles.logo}></img>
+        </figure>
+        <nav className={styles.menu}>
+          <ul className={styles.menuList}>
+            <li className={styles.menuItem}>
+              <Link linkName="Home" />
+            </li>
+            <li className={styles.menuItem}>
+              <Link linkName="Products" />
+            </li>
+            <li className={styles.menuItem}>
+              <Link linkName="Hot Deals" />
+            </li>
+            <li className={styles.menuItem}>
+              <Link linkName="About" />
+            </li>
+            <li className={styles.menuItem}>
+              <Link linkName="Contact" />
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.iconWrapper}>
+          <img src={SearchIcon} className={styles.icon}></img>
+          <img src={UserIcon} className={styles.icon}></img>
+          <img src={CartIcon} className={styles.icon}></img>
+        </div>
+      </section>
+    );
   }
 }
